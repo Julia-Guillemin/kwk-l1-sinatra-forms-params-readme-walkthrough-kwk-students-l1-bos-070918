3c69b7683d111a831f835f603730f0a3d0a272a1
Julia Guillemin <julia.guillemingirls@gmail.com>
2018-07-19 12:27:09 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,11 +1,18 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
-
+  
+  gets '/'do
+    "this is a home page"
+  end
+  
   get '/food_form' do
     erb :food_form
   end
 
+post '/food' do
+  "my name is #{params[:name]}, and i love #{params[:favorite_food]}."
+end
   # Add your post route and action below
 
 end
